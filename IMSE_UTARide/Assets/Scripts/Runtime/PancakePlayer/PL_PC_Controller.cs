cb1585553304cda61890b098ab88af0f018d588e
Catelluss <thecatellus@gmail.com>
2020-03-19 03:10:35 -0500

    Filestructure, PancakePlayer

---
+++ IMSE_UTARide/Assets/Scripts/Runtime/PancakePlayer/PL_PC_Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PL_PC_Controller : MonoBehaviour
+{
+    private PL_PC_Input input;
+    public Camera cam;
+    public CharacterController cc;
+    public float walkSpeed = 4;
+    public float gravity   = 10;
+    private float curGravity = 0;
+
+    void Start()
+    {
+        input = PL_PC_Input.Instance;
+        if (cc == null)
+            cc = this.gameObject.GetComponent<CharacterController>() ?? this.gameObject.AddComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (cc.isGrounded)
+            curGravity = 0;
+        HandleRotation();
+        HandleMovement();
+    }
+
+    void HandleRotation()
+    {
+        this.gameObject.transform.rotation = Quaternion.Euler(0, input.yaw, 0);
+        cam.transform.localRotation = Quaternion.Euler(input.pitch, 0, 0);
+    }
+
+    void HandleMovement()
+    {
+        Vector3 fwd = new Vector3(transform.forward.x, 0, transform.forward.z).normalized * input.movementInput.y;
+        Vector3 rit = new Vector3(transform.right.x, 0, transform.right.z).normalized * input.movementInput.x;
+        Vector3 final = (fwd + rit).normalized * walkSpeed;
+        final += new Vector3(0, (curGravity -= gravity * Time.deltaTime), 0);
+        cc.Move(final * Time.deltaTime);
+    }
+
+}
+
---
+++ IMSE_UTARide/Assets/Scripts/Runtime/PancakePlayer/PL_PC_Input.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PL_PC_Input : MonoBehaviour
+{
+    public static PL_PC_Input Instance { get; private set; }
+
+    public Vector2 movementInput { get; private set; } = Vector2.zero;
+    public float pitch { get; private set; } = 0;
+    public float yaw   { get; private set; } = 0;
+
+    public bool DEBUG_lockMouse = false;
+    public float camPitchSensativity = 1;
+    public float camYawSensativity = 1;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Debug.LogWarning($"{this.gameObject.name} has a duplicate PL_PC_Input component");
+    }
+
+    void Update()
+    {
+        movementInput = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        pitch = Mathf.Clamp(pitch - Input.GetAxisRaw("Mouse Y") * camPitchSensativity, -89, 89);
+        yaw = (yaw + Input.GetAxisRaw("Mouse X") * camYawSensativity) % 360;
+    }
+
+    private void OnValidate()
+    {
+        if (Application.isPlaying)
+        {
+            Cursor.visible = !DEBUG_lockMouse;
+            Cursor.lockState = DEBUG_lockMouse ? CursorLockMode.Locked : CursorLockMode.None;
+        }
+    }
+
+}
