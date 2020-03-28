a31f0b800c5fdce1688f20af42cd4f4b3f4c18ad
Jonah Bui <55065339+Jonahbui@users.noreply.github.com>
2020-03-28 13:48:06 -0500

    Add files via upload
    Added UI scripts

---
+++ IMSE_UTARide/Assets/Scripts/Runtime/UI_Scripts/UI_CanvasFollowObject.cs
@@ -0,0 +1,58 @@
+﻿/* Author: Jonah Bui
+ * Contributors:
+ * Date: March 24, 2020
+ * ------------------------
+ * Purpose: Make a list of GameObjects containing canvases follow an object.
+ * Changelog:
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_CanvasFollowObject: MonoBehaviour
+{
+    [Header("GameObjects for UI")]
+    [Tooltip("GameObjects containing canvases for the UI")]
+    public List<GameObjectUI> UICanvasGameObject = new List<GameObjectUI>();
+    [Tooltip("The parent object the UI will follow")]
+    public GameObject attachedObject;
+    void Start()
+    {
+        foreach (GameObjectUI obj in UICanvasGameObject)
+        {
+            // Make the canvas a child of the object to be attached to
+            // Ensures the canvas follows the tablet as it moves
+            obj.canvasGO.transform.SetParent(attachedObject.transform, false);
+
+            UI_ZeroOutTransform(obj.canvasGO.transform);
+            UI_SetTransformToObject(obj);
+        }
+    }
+
+    void Update()
+    {
+    }
+
+    // Zeros out the given transform
+    void UI_ZeroOutTransform(Transform t)
+    {
+        t.localPosition = Vector3.zero;
+        t.localRotation = Quaternion.Euler(0f, 0f, 0f);
+    }
+
+    void UI_SetTransformToObject(GameObjectUI g)
+    {
+        foreach (Transform child in g.canvasGO.transform)
+        {
+            RectTransform rect = child.GetComponentInChildren<RectTransform>();
+            if (rect != null)
+            {
+                // Scale the UI element to fit the attached object (tablet)
+                rect.localScale = g.scale;
+                // Position the UI element relative to the attached GameObject
+                rect.localPosition = g.position;
+            }
+        }
+    }
+}
---
+++ IMSE_UTARide/Assets/Scripts/Runtime/UI_Scripts/UI_InputMapping.cs
@@ -0,0 +1,33 @@
+﻿/* Author: Jonah Bui
+ * Contributors:
+ * Date: March 27, 2020
+ * ------------------------
+ * Purpose: Bindings for each of the possible inputs. Easy to change binds in a centralized file
+ * Changelog:
+ */
+using UnityEngine;
+
+public class UI_InputMapping 
+{
+    // Keyboard and Mouse Bindings
+    public static KeyCode ToggleMenuVisibility = KeyCode.Tab;
+
+    public static float VerticalInput = Input.GetAxis("Vertical");
+    public static float HorizontalInput = Input.GetAxis("Horizontal");
+
+    // Oculus Mappings
+    public static OVRInput.Button OVR_ToggleMenuVisibilty = OVRInput.Button.Start;
+    public static OVRInput.Button OVR_A = OVRInput.Button.One;
+    public static OVRInput.Button OVR_B = OVRInput.Button.Two;
+    public static OVRInput.Button OVR_X = OVRInput.Button.Three;
+    public static OVRInput.Button OVR_Y = OVRInput.Button.Four;
+    public static OVRInput.Button OVR_Start = OVRInput.Button.Start;
+
+    public static OVRInput.Axis1D OVR_LeftIndexTrigger = OVRInput.Axis1D.PrimaryIndexTrigger;
+    public static OVRInput.Axis1D OVR_RightIndexTrigger = OVRInput.Axis1D.SecondaryIndexTrigger;
+    public static OVRInput.Axis1D OVR_LeftHandTrigger = OVRInput.Axis1D.PrimaryHandTrigger;
+    public static OVRInput.Axis1D OVR_RightHandTrigger = OVRInput.Axis1D.SecondaryHandTrigger;
+
+    public static OVRInput.Axis2D OVR_LeftJoyStick = OVRInput.Axis2D.PrimaryThumbstick;
+    public static OVRInput.Axis2D OVR_RightJoyStick = OVRInput.Axis2D.SecondaryThumbstick;
+}
