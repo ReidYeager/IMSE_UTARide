7ca6e50afc3ea40b8351392156265de66c2a43a8
Mao Zhang <zoom@host-216-91.txarmav.arlington.tx.us.clients.pavlovmedia.net>
2020-04-09 15:16:14 -0500

    add VehicleManger demo

---
+++ IMSE_UTARide/Assets/Scripts/Vehicle Mananger/VM_Destructor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VM_Destructor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.transform.gameObject.tag == "Destructor"){
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ IMSE_UTARide/Assets/Scripts/Vehicle Mananger/VM_Generator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VM_Generator : MonoBehaviour
+{
+   // generator position
+    private Vector3 generatorPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    // vehicle prefab
+    public GameObject[] CarPrefab = new GameObject[4];
+    public GameObject[] BusPrefab = new GameObject[2];
+    public GameObject PoliceCarPrefab;
+    public GameObject TaxiPrefab;
+  
+
+    // generating timer
+    public float timeGap = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Generate", 0.0f, 20.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Generate(){
+        // int i = (int)Mathf.Floor(Random.Range(0.0f, timeGap)); 
+        // switch(i){
+        //     case 0: 
+        //         GameObject.Instantiate(CarPrefab[0], generatorPosition, Quaternion.identity); break;
+        //     case 1: 
+        //         GameObject.Instantiate(CarPrefab[1], generatorPosition, Quaternion.identity); break;
+        //     case 2: 
+        //         GameObject.Instantiate(CarPrefab[2], generatorPosition, Quaternion.identity); break;
+        //     case 3: 
+        //         GameObject.Instantiate(CarPrefab[3], generatorPosition, Quaternion.identity); break;
+        //     case 4: 
+        //         GameObject.Instantiate(BusPrefab[0], generatorPosition, Quaternion.identity); break;
+        //     case 5: 
+        //         GameObject.Instantiate(BusPrefab[1], generatorPosition, Quaternion.identity); break;
+        //     case 6: 
+        //         GameObject.Instantiate(PoliceCarPrefab, generatorPosition, Quaternion.identity); break;
+        //     case 7: 
+        //         GameObject.Instantiate(TaxiPrefab, generatorPosition, Quaternion.identity); break;
+        //     defaulf: break;
+        // }
+
+        GameObject.Instantiate(BusPrefab[0], generatorPosition, Quaternion.identity);
+    }
+}
