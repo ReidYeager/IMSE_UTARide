65c1af583f6b6a8cb6f8793bee56647d0007c881
ReidYeager <reidyeager@gmail.com>
2020-05-22 16:15:48 -0500

    Initial world state init static script to handle world state information - weather - time of day

---
+++ IMSE_UTARide/Assets/Scripts/Weather/WorldState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldState
+{
+    public static WorldState instance { get; private set; }
+
+    public enum weatherStates {
+        sunny = 1,
+        rainy = 2
+    }
+
+    public double timeOfDay = 0.0f; // Number of seconds starting at midnight(0.0f) to 11:59:59 (86399)
+
+}
